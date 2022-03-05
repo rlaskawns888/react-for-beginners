566706e0e36f35280e4592a2c04200f54db5cf85
knj <rlaskawns888@gmail.com>
2022-03-05 16:16:36 +0900

    cleanup function - react

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Getting Started with Create React App
+# nomardcorder - ReactJS 영화 서비스 만들기
+
+Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import Button from './Button';
-import styles from "./App.module.css";
-import { useState, useEffect } from 'react';
+/**
+ * 
+ * Cleanup
+ * 
+ */
+import { useState, useEffect } from "react";
+
+function Hello() {
+
+  useEffect(() => {
+    console.log('created :)');
+    return () => console.log("destoryed :(");
+  }, []);
+
+  // function byFn() {
+  //   console.log("bye :(");
+  // }
+  // function hiFn() {
+  //   console.log('created :)');
+  //   return byFn;
+  // }  
+  // useEffect(hiFn, []);
+
+  return <h1>Hello</h1>;
+}
+// Hello 컴포넌트가 사라질때 함수를 사용가능함. ex)click event 와 같은 이벤트 삭제가능.
 
 function App() {
-  const [counter, setConter] = useState(0);
-  const [keyword, setKeyWord] = useState("");
-  const onClick = () => setConter((prev) => prev + 1);
-  const onChange = (e)  => setKeyWord(e.target.value);
-  console.log('i run all the time');
-  useEffect(() => {
-    console.log('I run only once');
-  }, []);
-  // useEffect(1, 2)
-  // 1: 한번만 실행할 부분을 적는다.
-  // 2: [] 을 사용시, 한번만 실행됨.
-
-  useEffect(() => {    
-    console.log('I run when "keyword" changes', keyword);
-  }, [keyword]);
-  // 2 : 변화될떄만 동작하도록 설정 가능.
-  // ex) 위의 keyword 가 변화될때만, 실행 하도록 처리
-
-  useEffect(() => {
-    console.log("I run when 'counter' changes ", counter);
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'counter' & 'keyword'");
-  }, [keyword, counter]);
-  
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input 
-        value={keyword}
-        onChange={onChange} 
-        type="text" 
-        placeholder='Search here...'
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Counter Btn</button>
+      { showing ? <Hello /> : null }
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
