15aa9921822a8e34937f8928c133d7c92bc3cd92
DESKTOP-R701SCS\knj <jiji9859@naver.com>
2022-03-05 15:49:52 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './Button';
+import styles from "./App.module.css";
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setConter] = useState(0);
+  const [keyword, setKeyWord] = useState("");
+  const onClick = () => setConter((prev) => prev + 1);
+  const onChange = (e)  => setKeyWord(e.target.value);
+  console.log('i run all the time');
+  useEffect(() => {
+    console.log('I run only once');
+  }, []);
+  // useEffect(1, 2)
+  // 1: 한번만 실행할 부분을 적는다.
+  // 2: [] 을 사용시, 한번만 실행됨.
+
+  useEffect(() => {    
+    console.log('I run when "keyword" changes', keyword);
+  }, [keyword]);
+  // 2 : 변화될떄만 동작하도록 설정 가능.
+  // ex) 위의 keyword 가 변화될때만, 실행 하도록 처리
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes ", counter);
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter' & 'keyword'");
+  }, [keyword, counter]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder='Search here...'
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Counter Btn</button>
     </div>
   );
 }
